46ad5ad503e61869a5af0a040e4cb8ab81272300
Romast38 <40204473+Romast38@users.noreply.github.com>
2018-07-01 03:05:43 +0900

    Add files via upload

---
+++ Assets/RomaSimpleAnim.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class RomaSimpleAnim : MonoBehaviour
+{
+    SimpleAnimation sa;
+    string[] name;
+    int cnt = 0;
+
+    private void Start()
+    {
+        sa = GetComponent<SimpleAnimation>();
+        if (sa == null)
+        {
+            sa = gameObject.AddComponent<SimpleAnimation>();
+        }
+
+        AnimationClip[] clips = Resources.LoadAll<AnimationClip>("animation_clip");
+        name = new string[clips.Length];
+
+        for (int i = 0; i < clips.Length; i++)
+        {
+            var c = clips[i];
+            name[i] = c.name;
+            sa.AddClip(c, name[i]);
+        }
+
+        sa.Play(name[cnt]);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            cnt++;
+            if (cnt > name.Length - 1)
+            {
+                cnt = 0;
+            }
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            cnt--;
+            if (cnt < 0)
+            {
+                cnt = name.Length - 1;
+            }
+        }
+
+        if(Input.anyKeyDown)
+        {
+            sa.Play(name[cnt]);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 640, 480), name[cnt]);
+    }
+}
